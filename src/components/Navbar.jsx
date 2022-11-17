0c5cd24cff09079a6572337e3c1a36eea1c6c9f3
Shubham Mishra <shubhamishra583@gmail.com>
2022-11-17 22:43:20 +0530

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,24 @@ import React from 'react'
 import { useEffect } from "react";
 import { useState } from "react";
 import logo from "../assests/logo.png";
+import logomob from "../assests/logomob.png";
+import hamburger from "../assests/hamburger.png";
 import icon from "../assests/icon.png";
 import phn from "../assests/phn.png";
 import {NavLink } from "react-router-dom";
 import '../style/Navbar.css';
 import ScrollToTop from './ScrollToTop';
+import Logo from './Logo';
+import whatsapp from "../assests/whatsapp.png";
+
 
 const Navbar = () => {
     const [colorChange, setColorchange] = useState(false);
+    const [active, setActive] = useState(false);
+
+    const showNavbar = () =>{
+        setActive(!active)
+    }
 
     const changeNavbarColor = () => {
       if (window.scrollY >= 200) {
@@ -26,8 +36,10 @@ const Navbar = () => {
         <div className={colorChange ? "nav_colorChange" : "nav"}>
                   {/* <div className="nav"> */}
                   <div className="outer">
-                    <div>
+                    <div className='companylogo'>
                       <img src={logo} />
+                      <img src={logomob} />
+                      {/* <Logo/> */}
                     </div>
 
                     <div className="inner">
@@ -54,7 +66,11 @@ const Navbar = () => {
                       <NavLink to="/contactus">ContactUs </NavLink>
                       </li>
 
-                      <div className="phn">
+                    </div>
+
+
+                    {/* Start of login button */}
+                    <div className="phn">
                         <div>
                           <img src={phn} alt="" />
                         </div>
@@ -62,31 +78,51 @@ const Navbar = () => {
                           <a href="tel:65123456789">+65 123 456 789</a>
                         </div>
                       </div>
-                    </div>
+                    <img src = {hamburger} onClick = {showNavbar} className = 'hamburger' alt="" />
 
-                    <div className="login">
-                      <a href="">
-                        {/* <div><img src={icon} alt="" /></div> */}
-                        <svg
-                          width="17"
-                          height="16"
-                          viewBox="0 0 17 16"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path className="icon"
-                            d="M8.5 8C10.71 8 12.5 6.21 12.5 4C12.5 1.79 10.71 0 8.5 0C6.29 0 4.5 1.79 4.5 4C4.5 6.21 6.29 8 8.5 8ZM8.5 10C5.83 10 0.5 11.34 0.5 14V15C0.5 15.55 0.95 16 1.5 16H15.5C16.05 16 16.5 15.55 16.5 15V14C16.5 11.34 11.17 10 8.5 10Z"
-                            fill="white"
-                          />
-                        </svg>
-                        {/* <div>CLIENT LOGIN</div> */}
-                        CLIENT LOGIN
-                      </a>
-                    </div>
+                      <div className = {active ? "activenavmenu" : "navmenu"}>
+                            <div className="menuitems">
+                              {/* Links */}
+
+                              <li>
+                              <NavLink exact to="/">Home</NavLink>
+                              </li>
+                              {/* <li><a href="">Services</a></li> */}
+                              <li>
+                                <NavLink to="/services">Services </NavLink>
+                              </li>
+
+                              <li>
+                                <NavLink to="/gallery">Gallery </NavLink>
+                              </li>
+                              <li>
+                              <NavLink to="/blog">Blog </NavLink>
+                              </li>                      
+                              <li>
+                              <NavLink to="/careers">Careers </NavLink>
+                              </li>
+                              <li>
+                              <NavLink to="/contactus">ContactUs </NavLink>
+                              </li>
+
+                              <a
+                                href="https://wa.me/65123456789"
+                                target="_blank"
+                                className="whatsapp"
+                              >
+                                <img src={whatsapp} alt="" />
+                              </a>
+
+                            </div>
+                      </div>
+
                   </div>
+
+                  
                 </div>
     </div>
   )
 }
 
 export default Navbar
+
